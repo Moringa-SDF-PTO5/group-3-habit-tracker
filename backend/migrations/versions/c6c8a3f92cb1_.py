1e6a5fd97b9f31ea979690b25c36101b383f2fbb
Njehia98 <stacynjihia028@gmail.com>
2024-06-24 20:58:56 +0300

    trying to deploy

---
+++ backend/migrations/versions/c6c8a3f92cb1_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c6c8a3f92cb1
+Revises: 
+Create Date: 2024-06-24 20:46:09.402917
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c6c8a3f92cb1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
