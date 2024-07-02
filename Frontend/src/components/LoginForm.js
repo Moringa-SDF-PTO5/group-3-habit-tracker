f64f13ea8caa19a1c7167dd5c8d0b8d713e6491a
Alex Njoroge <alex.njoroge@student.moringaschool.com>
2024-07-02 00:28:01 -0700

    Implemented Login Function

--- Frontend/src/components/LoginForm.js
+++ Frontend/src/components/LoginForm.js
@@ -17,24 +17,51 @@ const LoginForm = ({ onLogin }) => {
         }
         return errors;
       }}
-      onSubmit={(values, { setSubmitting }) => {
-        // Add authentication logic here
-        onLogin(values.username);
-        setSubmitting(false);
+      onSubmit={(values, { setSubmitting, setErrors }) => {
+        const requestData = {
+          username: values.username,
+          password_hash: values.password, // Using 'password_hash' as the key
+        };
+
+        fetch('https://backend1-1-m0ph.onrender.com/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(requestData),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              return response.json().then((data) => {
+                throw new Error(data.message || 'Login failed');
+              });
+            }
+            return response.json();
+          })
+          .then((data) => {
+            onLogin(data.username);
+            setSubmitting(false);
+          })
+          .catch((error) => {
+            setErrors({ server: error.message });
+            setSubmitting(false);
+          });
       }}
     >
-      {({ isSubmitting }) => (
+      {({ isSubmitting, errors }) => (
         <Form className="login-form">
           <Field type="text" name="username" placeholder="Username" />
           <ErrorMessage name="username" component="div" className="error-message" />
           
           <Field type="password" name="password" placeholder="Password" />
           <ErrorMessage name="password" component="div" className="error-message" />
+
+          {errors.server && <div className="error-message">{errors.server}</div>}
           
           <button type="submit" disabled={isSubmitting}>
             Login
           </button>
-          <Link to = 'register' >No account ? Register Here</Link>
+          <Link to='register'>No account? Register Here</Link>
         </Form>
       )}
     </Formik>
